6cffb8fa81402a2960e5c3be392e92676b5b363b
Andrew Kairys <andrewkairys@gmail.com>
2018-03-08 13:17:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
-def prime?(number)
-  if number < 0 || number == 0 || number == 1
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
     return false
   else
-    false
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
