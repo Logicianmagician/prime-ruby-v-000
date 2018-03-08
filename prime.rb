50b97e955f6150811cf2406c7f375e1caded06b3
Andrew Kairys <andrewkairys@gmail.com>
2018-03-08 13:38:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,6 @@ def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
-    (2..num-1).to_a.all? do |i|
-      num % i != 0
-    end
+    (2..num-1).to_a.all? { |i| num % i != 0}
   end
 end
